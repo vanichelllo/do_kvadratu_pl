b2084ca81971c0a25bdae1873b72abd2ecaefbcb
Ivan Sklianchuk <isklyanchuk5@gmail.com>
2026-07-21 23:49:34 +0300

    Дебаг реєстрації

--- do_kvadratu/settings.py
+++ do_kvadratu/settings.py
@@ -190,7 +190,7 @@ MONOBANK_TOKEN = os.getenv('MONOBANK_TOKEN')
 # ==========================================
 # НАЛАШТУВАННЯ ПОШТИ (SMTP GMAIL)
 # ==========================================
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = False
