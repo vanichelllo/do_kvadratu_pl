60a1c646cb9b8880c9d83ce980da10bdd6e3009c
Ivan Sklianchuk <isklyanchuk5@gmail.com>
2026-07-24 00:33:22 +0300

    Додаємо бот в тг

--- telegram_bot/views.py
+++ telegram_bot/views.py
@@ -2,26 +2,29 @@ import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
-from asgiref.sync import async_to_sync
 from aiogram import Bot, Dispatcher, types
 
 from .handlers import router
 
-bot = Bot(token=settings.TELEGRAM_BOT_TOKEN)
 dp = Dispatcher()
 dp.include_router(router)
 
 
 @csrf_exempt
-def telegram_webhook(request):  # Зверніть увагу: прибрали слово async!
+async def telegram_webhook(request):  # Повертаємо async, Django 4+ це чудово розуміє!
     if request.method == 'POST':
         try:
-            # Читаємо дані від Telegram
+            # Створюємо бота ТІЛЬКИ на час обробки повідомлення, щоб не було конфліктів у пам'яті
+            bot = Bot(token=settings.TELEGRAM_BOT_TOKEN)
+
             json_data = request.body.decode('utf-8')
             update = types.Update.model_validate_json(json_data, context={"bot": bot})
 
-            # Магія: запускаємо асинхронного бота в синхронному середовищі Django
-            async_to_sync(dp.feed_update)(bot, update)
+            # Обробляємо повідомлення
+            await dp.feed_update(bot, update)
+
+            # Правильно закриваємо з'єднання
+            await bot.session.close()
 
         except Exception as e:
             print(f"Помилка Webhook: {e}")
